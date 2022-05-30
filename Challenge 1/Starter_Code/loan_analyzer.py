be1345c365763e312e388d7a2b5e13d5a18decb9
JWM09 <105887423+JWM09@users.noreply.github.com>
2022-05-30 16:47:55 -0700

    Add files via upload
    upload with half of problem 3 (function statement) working

--- Challenge 1/Starter_Code/loan_analyzer.py
+++ Challenge 1/Starter_Code/loan_analyzer.py
@@ -2,7 +2,7 @@
 import csv
 from pathlib import Path
 
-from sympy import re
+from sympy import fu, re
 
 """Part 1: Automate the Calculations.
 
@@ -109,8 +109,13 @@ Perform financial calculations using functions.
 2. Use the function to calculate the present value of the new loan given below.
     a. Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 """
+def loan_pv(future_value, remaining_months, annual_discount_rate):
+  
+    present_value =  future_value / (1 + annual_discount_rate / 12) ** remaining_months
+    print(present_value)
 
 # Given the following loan data, you will need to calculate the present value for the loan
+
 new_loan = {
     "loan_price": 800,
     "remaining_months": 12,
@@ -122,13 +127,15 @@ new_loan = {
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
+annual_discount_rate = 0.20
+loan_present_value = loan_pv(new_loan["future_value"], new_loan["remaining_months"], annual_discount_rate)
+print(loan_present_value)
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
-
+print(f"The present value of the loan is: {loan_present_value}")
 
 """Part 4: Conditionally filter lists of loans.
 
